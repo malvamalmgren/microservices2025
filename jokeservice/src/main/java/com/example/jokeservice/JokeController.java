3ad886aada6f91bfbdc55fe8a267b55c0fe81c99
Malva Malmgren <malva.malmgren@gmail.com>
2025-05-30 20:24:36 +0200

    Add and implement jokeservice

--- jokeservice/src/main/java/com/example/jokeservice/JokeController.java
+++ jokeservice/src/main/java/com/example/jokeservice/JokeController.java
@@ -1,18 +1,24 @@
 package com.example.jokeservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
+@RequestMapping("/jokes")
 public class JokeController {
+    private final Random random = new Random();
 
-    @GetMapping("/secure")
-    public String getSecureJoke() {
-        return "Secure Joke Server, valid token";
-    }
+    private final String[] jokes = {
+            "Why don't scientists trust atoms? Because they make up everything!",
+            "I told my computer I needed a break, and it said 'No problem – I'll go to sleep.'",
+            "Why did the scarecrow win an award? Because he was outstanding in his field!"
+    };
 
-    @GetMapping("/public")
-    public String getPublicJoke() {
-        return "Public Joke Server";
+    @GetMapping("/random")
+    public String getJoke() {
+        return jokes[random.nextInt(jokes.length)];
     }
 }
--- jokeservice/src/main/java/com/example/jokeservice/JokeServiceApplication.java
+++ jokeservice/src/main/java/com/example/jokeservice/JokeServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -15,34 +16,16 @@ import java.util.Arrays;
 @SpringBootApplication
 public class JokeServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JokeServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JokeServiceApplication.class, args);
+    }
 
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
-		http
-				.cors(cors -> cors.configurationSource(corsConfigurationSource))
-				.authorizeHttpRequests(authorize -> authorize
-						.requestMatchers("/secure").hasAuthority("SCOPE_read_resource") //or .authenticated()
-						.requestMatchers("/public").permitAll()
-						.anyRequest().authenticated()
-				)
-				.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
-		return http.build();
-	}
-
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:8888"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		configuration.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-		configuration.setAllowCredentials(true);
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
-
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+        http.authorizeHttpRequests(authorize ->
+                authorize.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
 }
